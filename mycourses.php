890698a322cb959bb00707144cce85d78e040d7b
piyushgwayamaru <156769305+piyushgwayamaru@users.noreply.github.com>
2025-09-02 13:36:55 +0545

    Update mycourses.php

--- mycourses.php
+++ mycourses.php
@@ -2,191 +2,123 @@
 
 if (isset($_SESSION['SESSION_EMAIL'])){
     $email = $_SESSION['SESSION_EMAIL'];
+} else {
+    // If user is not logged in, redirect them to the login page
+    header('Location: log-in.php');
+    exit();
 }
 ?>
 
-	<!--breadcrumb starts-->
-	<div class="breadcrumb-nav">
-		<div class="container">
-			<nav aria-label="breadcrumb">
-				<ol class="breadcrumb mb-0">
-					<li class="breadcrumb-item"><a href="index.php">Home</a></li>
-					<li class="breadcrumb-item active" aria-current="page">My courses</li>
-				</ol>
-			</nav>
-		</div>
-	</div>
-	<!--breadcrumb ends-->
+<!--breadcrumb starts-->
+<div class="breadcrumb-nav">
+    <div class="container">
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb mb-0">
+                <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+                <li class="breadcrumb-item active" aria-current="page">My Courses</li>
+            </ol>
+        </nav>
+    </div>
+</div>
+<!--breadcrumb ends-->
 
-	<?php
-			function realNum($num) {
-				$num1 = floor($num);
-				$num2 = $num1+.5;
-				$num3 = $num1+1;
-				if (($num >= $num1) && ($num < $num2)) {
-					$answer = $num1;
-				} 
-				elseif (($num >= $num2) && ($num < $num3)) {
-					$answer = $num2;
-				}
-				return $answer;
-			}
-			?>
-
-	<!--course section starts-->
-	<section class="course-section section-padding">
-		<div class="container">
-			<h2>My Courses</h2><br>
-		<div class="row">
-		<div class="col-12">
-			
-			
-				<div class="tab-pane fade show <?php if($cc == 0){ echo 'active'; }?> " id="<?php echo str_replace(' ','-',$name)?>" role="tabpanel" aria-labelledby="web-development-tab">
-				<div class="row">
-				<?php
-				$sql="SELECT *, tbl_course.id
-                FROM tbl_course
-                JOIN tbl_enroll ON tbl_course.id = tbl_enroll.course_id 
-                where email = '$email' AND status=1 AND active='Yes'";
-				$res2=mysqli_query($conn,$sql);
-
-				$count2=mysqli_num_rows($res2);
-
-				if($count2>0)
-				{
-					while($row2=mysqli_fetch_assoc($res2))
-					{
-						$id=$row2['id'];
-						$title=$row2['title'];
-						$image_name=$row2['image'];
-						$instructor_id=$row2['educator_id'];
-						$price=$row2['price'];
-				?>
-				<!-- For the ratings read from review table -->
 <?php
-	$sum =0;
-	$count =0;
-    $count1 =0;
-    $count2 =0;
-    $count3 =0;
-    $count4 =0;
-    $count5 =0;
-	
-	$sql="SELECT * FROM tbl_reviews where course_id = $id";
-	if($result=mysqli_query($conn,$sql))
-	{
-		if(mysqli_num_rows($result)>0)
-		{
-			while($row=mysqli_fetch_array($result))
-			{
-				
-				$rating_data = $row['rating_data'];
-                if($rating_data == 1){
-                    $count1++;
-                }
-                if($rating_data == 2){
-                    $count2++;
-                }
-                if($rating_data == 3){
-                    $count3++;
-                }
-                if($rating_data == 4){
-                    $count4++;
-                }
-                if($rating_data == 5){
-                    $count5++;
-                }
-				$sum += $rating_data;
-				$count++;
-				
-			}
-		}
-	}
-
-	if($count !=0){
-		$real_rating = $sum/$count;
-	} else{
-		$real_rating = 0;
-	}
-	
-	// echo $real_rating;
-	$real_rating_final = realNum($real_rating);
-
+function realNum($num) {
+    $num1 = floor($num);
+    $num2 = $num1 + 0.5;
+    if ($num >= $num2) {
+        return $num1 + 0.5;
+    }
+    return $num1;
+}
 ?>
-				<!--courses item starts-->
 
-				<div class="">
-					<div class="courses-item">
-						<a href="watch.php?course_id=<?php echo $id; ?>" class="link">
-							<div class="m-inner">
-								<div class="img-box">
-									<img src="img/admin/course/<?php echo $image_name; ?>" alt="course img" style="height:175px; width:300px;">
-								</div>
-								<h3 class="title"><?php echo $title;?></h3>
-								<div class="instructor">
-									<!-- <img src="img/instructor/1.png" alt="insrtucting"> -->
-									<span class="instructor-name"><?php 
-									$res1 = mysqli_query($conn, "SELECT name FROM tbl_educator WHERE id='$instructor_id'");
-									$row1 = mysqli_fetch_assoc($res1);
-									echo $row1['name']; ?> </span>
-								</div>
-								<div class="rating">
-									<span class="average-rating">(<?php echo $real_rating_final ?>)</span>		
-									<span class="average-stars">
-										<?php
-											if ($real_rating_final == 0){
-												for ($i = 1; $i <= 5; $i++) {
-													?>
-													<span class="average-stars">
-														<i class="fa-regular fa-star"></i>
-													</span>
-													<?php
-												}
-											}
-											for ($i = 1; $i <= floor($real_rating_final); $i++) {
-												?>
-												<span class="average-stars">
-												<i class="fas fa-star"></i>
+<!--course section starts-->
+<section class="course-section section-padding">
+    <div class="container">
+        <h2>My Courses</h2><br>
+        <div class="row">
+            <?php
+            // SQL query to get all courses the logged-in user has successfully enrolled in
+            $sql = "SELECT *, tbl_course.id
+                    FROM tbl_course
+                    JOIN tbl_enroll ON tbl_course.id = tbl_enroll.course_id 
+                    WHERE email = '$email' AND status = 1 AND active = 'Yes'";
+            
+            $res = mysqli_query($conn, $sql);
+            $count_courses = mysqli_num_rows($res);
 
-												</span>
-												<?php
-											}
-											$rem = $i-$real_rating_final;
-											if($rem==0.5){
-												?>
-												<span class="average-stars">
-												<i class="fas fa-star-half-alt"></i>
-												</span>
-												<?php
-											}
-										?>
-									</span>
-									<?php
-									}
-									?>
-								</div>
-							</div>
-						</a>
-					</div>
-				</div>
+            if ($count_courses > 0) {
+                while ($row = mysqli_fetch_assoc($res)) {
+                    // Get course details
+                    $id = $row['id'];
+                    $title = $row['title'];
+                    $image_name = $row['image'];
+                    $instructor_id = $row['educator_id'];
 
-							<!--courses item ends-->
-				<?php 
-					}
-				
-				?> 
-			</div>
-		</div>				 
-	
-	</div>
-</div>
-</div>
-				
-					</div>
-				</div>
-			</div>
-		</section>
-		<!--course section ends-->
+                    // --- Calculate average rating for this course ---
+                    $sum = 0;
+                    $count_reviews = 0;
+                    $sql_reviews = "SELECT rating_data FROM tbl_reviews WHERE course_id = $id";
+                    $result_reviews = mysqli_query($conn, $sql_reviews);
+                    if (mysqli_num_rows($result_reviews) > 0) {
+                        while ($row_review = mysqli_fetch_array($result_reviews)) {
+                            $sum += $row_review['rating_data'];
+                            $count_reviews++;
+                        }
+                    }
+                    $real_rating = ($count_reviews > 0) ? $sum / $count_reviews : 0;
+                    $real_rating_final = realNum($real_rating);
+                    ?>
 
-		<?php include('footer.php'); ?>
+                    <!--courses item starts-->
+                    <div class="col-md-6 col-lg-3">
+                        <div class="courses-item">
+                            <a href="watch.php?course_id=<?php echo $id; ?>" class="link">
+                                <div class="courses-item-inner">
+                                    <div class="img-box">
+                                        <img src="img/admin/course/<?php echo htmlspecialchars($image_name); ?>" alt="course img" style="height:175px; width:300px;">
+                                    </div>
+                                    <h3 class="title"><?php echo htmlspecialchars($title); ?></h3>
+                                    <div class="instructor">
+                                        <span class="instructor-name">
+                                            <?php 
+                                            $res1 = mysqli_query($conn, "SELECT name FROM tbl_educator WHERE id='$instructor_id'");
+                                            $row1 = mysqli_fetch_assoc($res1);
+                                            echo htmlspecialchars($row1['name']); 
+                                            ?>
+                                        </span>
+                                    </div>
+                                    <div class="rating">
+                                        <span class="average-rating">(<?php echo $real_rating_final; ?>)</span>		
+                                        <span class="average-stars">
+                                            <?php
+                                            for ($i = 1; $i <= 5; $i++) {
+                                                if ($i <= floor($real_rating_final)) {
+                                                    echo '<i class="fas fa-star"></i>';
+                                                } elseif ($real_rating_final > floor($real_rating_final) && $i == ceil($real_rating_final)) {
+                                                    echo '<i class="fas fa-star-half-alt"></i>';
+                                                } else {
+                                                    echo '<i class="far fa-star"></i>'; // Use far for empty star
+                                                }
+                                            }
+                                            ?>
+                                        </span>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <!--courses item ends-->
+            <?php
+                } // End while loop
+            } else {
+                echo "<p>You have not enrolled in any courses yet.</p>";
+            }
+            ?>
+        </div> <!-- End .row -->
+    </div>
+</section>
+<!--course section ends-->
 
+<?php include('footer.php'); ?>
